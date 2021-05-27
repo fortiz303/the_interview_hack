e4652de0215214a190552dc085adb0643b5df9e1
francisco <francisco_ortiz1@yahoo.com>
2021-05-27 14:59:33 -0600

    finished all questions

--- interview.py
+++ interview.py
@@ -1,9 +1,117 @@
 import time
 
+#This platform should have a "compare your answers with others." Also, there will be alink where interview preparees can click on and understand more about the answer to the interview question.
+
+print("This is a 20 minute interview designed to land you a job as a Scrum Master. Do this exercise at least two times per day before an interview. Recruiters want to know how you handle conflict and help the team deliver value. This exercise emphasizes that.")
+
+time.sleep(7)
+
+print("Remember that as you fill out this assignment, you have to Be Your Toughest Critic(BYWC). Are you finding that you're explaining yourself too much during the 'Tell me about you part?' Good! Do it over (To do this type 'go back')! ")
+
+#1
 you = input("Tell me about yourself in 20 seconds or less: ")
 
-time.sleep(20)
+#if you == "go back":
 
-if time.sleep(20) > time.sleep(19):
-	print("times out chuck!") 
+#2	
+daily_scrum = input("Tell me what happens in your Daily Scrum? ") 
+
+print("Did you make sure to include that your team discusses how to reach the Sprint Goal? If not, type 'go back.' Remember, Be Your Toughest Critic.")
+
+time.sleep(5)
+
+#3
+backlog_refinement = input("What happens in your backlog refinement?")
+
+print("Did you make sure to include that the backlog refinement is a meeting... finish later")
+
+time.sleep(5)
+
+#4
+sprint_review = input("What happens in your sprint review?")
+
+print("Did you make sure to include that the backlog refinement is a meeting... finish later")
+
+time.sleep(5)
+
+#5
+stakeholder_wants_feature = input("The PO approaches you and wants a new feature mid-sprint, how do you respond as the SM?")
+
+print("Did you include that you coached the PO into ...")
+
+time.sleep(5)
+
+#6
+competing_priorities = input("How do you handle multiple competing priorities?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+
+#7
+greatest_accomplishment = input("What is one of your greatest accomplishments with your team?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#8
+difficult_stakeholder = input("Name a time you had to deal with a difficult stakeholder and how did you resolve it?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#9
+process_change = input("Have you ever had to implement a process change and how did you accomplish it?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#10
+ceo = input("If a CEO ask you to show where you are at in the product what would you show them?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#11
+sdlc = input("Can you describe the SDLC and how scrum goes through it?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#12
+conflict_between_devs = input("How do you overcome the conflict between two strong developers?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#13
+
+items_undelivered = input("How do you deal with items not being delivered in a sizeable time?")
+
+more_agile = input("How would you help a team become more agile?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+#15
+large_stories = input("How would you handle an 8 point story? Do you break it up into multiple sprints etc?")
+
+print("Did you mention something along the lines of...")
+
+time.sleep(5)
+
+print("Conrats, you are done for the day. If you found these questions of great help, please consider donating.")
+
+#time.sleep(20)
+
+#if time.sleep(20) > time.sleep(19):
+#	print("times out chuck!") 
 
